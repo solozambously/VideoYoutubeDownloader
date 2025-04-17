4db157dbb9ea8659b613f53640fd406294769440
SoloZambously Developpement <zambouslysolo@gmail.com>
2025-04-17 15:51:23 +0200

    fix: cors politics

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from functions import *
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/download_audio', methods=['POST'])
 def download_audio_m4a_route():
