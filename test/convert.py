5330c945670d413dfccaba57d247754f9e563dda
SoloZambously Developpement <zambouslysolo@gmail.com>
2025-04-17 14:35:01 +0200

    delete test files

--- test/convert.py
+++
@@ -1,4 +0,0 @@
-from pydub import AudioSegment
-
-audio = AudioSegment.from_file(r"/home/zob/projects/yt_downloader_api/VideoYoutubeDownloader/test/test.m4a")
-audio.export(r"/home/zob/projects/yt_downloader_api/VideoYoutubeDownloader/test/test.mp3", format="mp3")
