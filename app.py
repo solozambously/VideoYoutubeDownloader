183868563c7ccc8c13a9199b69e0905e050afb56
SoloZambously Developpement <zambouslysolo@gmail.com>
2025-05-05 19:52:34 +0200

    Feat: ping

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/ping', methods=['GET'])
+def ping():
+    return jsonify('pong'), 200
+
 @app.route('/search_video', methods=['POST'])
 def search_video_route():
     search = request.form.get("search")
